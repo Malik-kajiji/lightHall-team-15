a83fc0d855f436ceac572cb1b639c76971bbfa26
AdvaitMadhekarOfficial <57474725+AdvaitMadhekarOfficial@users.noreply.github.com>
2023-04-19 13:17:28 -0700

    Update TaskCard.js

--- src/components/TaskCard.js
+++ src/components/TaskCard.js
@@ -7,13 +7,33 @@ import EditTask from './EditTask';
 // the type can be 'waitList' or 'inProgress' or 'completed' or 'expired'
 const TaskCard = ({taskType,taskId,taskTitle,taskDesc,TaskEndDate}) => {
     return (
-        <>
-            <EditTask />
-            <article>
-                
-            </article>
-        </>
-    )
+        <div className="card">
+            <div className="card-section">
+                <div className="card-header">
+                <h2 className="card-title">Task Title</h2>
+                <div className="card-buttons">
+                    <button className="card-button edit"><i className="fas fa-edit"></i></button>
+                    <button className="card-button delete"><i className="fas fa-trash-alt"></i></button>
+                </div>
+                </div>
+            </div>
+            <div className="card-section">
+                <div className="card-body">
+                <p>Task Information.</p>
+                </div>
+            </div>
+            <div className="card-section">
+                <div className="card-footer">
+                    <div className="card-footer-section">
+                        <p>End Date</p>
+                    </div>
+                    <div className="card-footer-section">
+                        <p>May 31, 2023</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default TaskCard
